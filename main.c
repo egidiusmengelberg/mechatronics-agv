13b5fda08460a9256c5765a5fc63877d51fac536
Egidius Mengelberg <mengelberg@mengelberg.eu>
2023-05-24 21:55:02 +0200

    wip

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
+#include <util/delay.h>
+
+
+#include "stepper.h"
 
 int main(void) {
 
+    initSteppers();
+
     while(1) {
-		
+		stepperLStep();
+        //_delay_ms(4);
 	}
 }
---
+++ stepper.h
@@ -0,0 +1,6 @@
+#include "config.h"
+#include <util/delay.h>
+
+void initSteppers();
+void stepperLStep();
+void stepperRStep();
